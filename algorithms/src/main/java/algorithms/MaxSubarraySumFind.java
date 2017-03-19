0e5be7d95f700e0b7005fa0582932a80c7bf1b61
ConstantineBoitsov <nyankostnyan@gmail.com>
2017-03-19 21:14:50 +0300

    Maximum subarray sum find

---
+++ algorithms/src/main/java/algorithms/MaxSubarraySumFind.java
@@ -0,0 +1,30 @@
+package algorithms;
+
+/**
+ * Created by Константин on 19.03.2017.
+ */
+
+public class MaxSubarraySumFind {
+
+    public static double findMaxSubarraySum(double [] array) {
+
+        double maxSum = array[0];
+        double lastSum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+
+            lastSum += array[i];
+
+            if (lastSum > maxSum) {
+                maxSum = lastSum;
+            }
+
+            if (lastSum < 0) {
+                lastSum = 0;
+            }
+        }
+        return maxSum;
+
+    }
+
+}
