f3ba92d3a06fc57cb620ad7efe2b5090ea5791fe
ConstantineBoitsov <nyankostnyan@gmail.com>
2017-03-24 17:24:15 +0300

    +2 test situations

--- algorithms/src/test/java/algorithms/MaxSubarraySumFinderTest.java
+++ algorithms/src/test/java/algorithms/MaxSubarraySumFinderTest.java
@@ -19,6 +19,12 @@ public class MaxSubarraySumFinderTest {
         array = new double[] {-100.88, 86.91, 91.58, -22.71, 44.55, 74.68, 28.07, 64.93};
         assertEquals(368.01, MaxSubarraySumFind.findMaxSubarraySum(array), 1e-3);
 
+        array = new double[] {-42.12, -10.44, -0.11, -85.81, -17.21, -0.16, -7.77, -1.07, -52.67, -49.97};
+        assertEquals(-0.11, MaxSubarraySumFind.findMaxSubarraySum(array), 1e-3);
+
+        array = new double[] {0.12, -4.52, 3.78, 0.32, -1.47, 2.03, 1.01, -5.68, 1.73, 0.13};
+        assertEquals(5.67, MaxSubarraySumFind.findMaxSubarraySum(array), 1e-3);
+
     }
 
 }
